0e9e63f0bd3ce6050c72203bf1f5d414498377cb
Maxim Shatalov <shatalov04@gmail.com>
2018-05-02 22:16:34 +0300

    #1-2, 4-6 tasks are completed

--- src/debug.js
+++ src/debug.js
@@ -1,8 +1,51 @@
-function bindFunction(fn, ...args) {
-    console.log(fn(...args));
-    return fn(...args);
+// // function slice(array, from = 0, to = array.length) {
+// //     let result = [];
+// //     for (let i = from; i < to; i++) {
+// //         result.push(array[i]);
+// //     }
+// //     return result;
+// // }
+// //
+// //
+// // console.log(slice([1, 2, 3, 4, 5]));
+//
+//
+// class ProxyObj {
+//     constructor() {
+//         return new Proxy(this, this);
+//     }
+//
+//     get(target, prop) {
+//         return this[prop] || '';
+//     }
+//
+//     set(target, prop, value) {
+//         this[prop] = value * value;
+//     }
+// }
+
+const handler = {
+    get: function (obj, prop) {
+        return obj[prop] * obj[prop];
+    }
+};
+
+function createProxy(obj) {
+    return new Proxy(obj, handler);
 }
 
-let sum = (a, b, c) => a + b + c;
 
-bindFunction(sum, 2, 4, 6);
+
+let obj = {};
+
+obj = createProxy(obj);
+
+console.log(obj);
+
+obj.x = 3;
+console.log(obj.x);
+
+obj.y = 4;
+
+console.log(obj.x);
+console.log(obj.y);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i, array);
+    }
 }
 
 /*
@@ -16,6 +19,13 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+    let result = [];
+    array.forEach(function (item, i, array) {
+        result.push(
+            fn(item, i, array)
+        );
+    });
+    return result;
 }
 
 /*
@@ -25,6 +35,11 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
+    let value = initial || array[0];
+    array.forEach(function (item, i, array) {
+        value = fn(value, item, i, array);
+    });
+    return value;
 }
 
 /*
@@ -33,9 +48,14 @@ function reduce(array, fn, initial) {
  Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
 
  Пример:
-   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+    let result = [];
+    for (let key in obj) {
+        result.push(key.toUpperCase());
+    }
+    return result;
 }
 
 /*
@@ -44,7 +64,22 @@ function upperProps(obj) {
  Напишите аналог встроенного метода slice для работы с массивами
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
-function slice(array, from, to) {
+function slice(array, from = 0, to = array.length) {
+    let result = [];
+    if (to < 0) {
+        to = array.length + to;
+    }
+    if (to > array.length) {
+        to = array.length;
+    }
+    if (from < 0) {
+        from = 0;
+    }
+
+    for (let i = from; i < to; i++) {
+        result.push(array[i]);
+    }
+    return result;
 }
 
 /*
@@ -53,8 +88,16 @@ function slice(array, from, to) {
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
+const handler = {
+    get: function (obj, prop) {
+        return obj[prop] * obj[prop];
+    }
+};
+
 function createProxy(obj) {
+    return new Proxy(obj, handler);
 }
+
 
 export {
     forEach,
